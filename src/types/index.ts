bcb7f4229b4193a1d4604bf2104d135da74e262a
nthapa000 <nishantthapa0000@gmail.com>
2024-04-28 13:44:35 +0530

    types

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface TableTypes {
+    stateid: number;
+    districtid: number;
+    State: string;
+    state_dist_key: string;
+    District: string;
+    year_val: number;
+    jan: number;
+    feb: number;
+    mar: number;
+    apr: number;
+    may: number;
+    jun: number;
+    jul: number;
+    aug: number;
+    sep: number;
+    oct: number;
+    nov: number;
+    dec: number;
+  }
+  
+  export interface Precipitation_2004_2011 {
+    State: "text";
+    District: "text";
+    year_val: "int";
+    January: "double";
+    February: "double";
+    March: "double";
+    April: "double";
+    May: "double";
+    June: "double";
+    July: "double";
+    August: "double";
+    September: "double";
+    October: "double";
+    November: "double";
+    December: "double";
+    AnnualTotal: "double";
+  }
+  
