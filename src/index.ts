fa8567b5eaf70359117270d0e192cfaca1a61645
nthapa000 <nishantthapa0000@gmail.com>
2024-04-28 14:34:13 +0530

    Checking DB connection and setting up CI github workflow

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,28 @@ import { TableTypes } from "./types"
 import { Precipitation_2004_2011 } from "./types"
 const app = express()
 app.use(cors())
+require('dotenv').config();
+
+app.get('/',(req:Request,res:Response)=>{
+    return res.json("From Backend side")
+})
+
+const db = mysql.createConnection({
+    host:process.env.DB_URL,
+    user:process.env.DB_User,
+    password:process.env.DB_password,
+    database:process.env.DB_database,
+})
+
+app.get('/state',(req:Request,res:Response)=>{
+    const sql = "SELECT DISTINCT State,stateid FROM practice.WetDayFrequency ORDER BY stateid ASC;"
+    db.query(sql,(err:Error|null,data:TableTypes[]) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+})
+
+app.listen(8001,()=>{
+    console.log("listening")
+})
 
